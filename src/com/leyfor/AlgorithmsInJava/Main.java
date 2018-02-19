64abd4632a11f43a3e74f73ff13228e6ac7a5e55
leyfor <justman51@yahoo.com>
2018-02-19 08:39:45 +0100

    Refactor for code reuse

--- src/com/leyfor/AlgorithmsInJava/Main.java
+++ src/com/leyfor/AlgorithmsInJava/Main.java
@@ -3,23 +3,8 @@ package com.leyfor.AlgorithmsInJava;
 public class Main {
 
     public static void main(String[] args) {
-        SelectionSort selectionSort = new SelectionSort();
-
         int[] intArray = {20, 35, -15, 7, 55, 1, -72};
-
-        // For Loop for Selection Sort
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            int largest = 0; // First index
-            for (int i = 1; i <= lastUnsortedIndex; i = i + 1) {
-                if (intArray[i] > intArray[largest]) {
-                    largest = i;
-                }
-            }
-            selectionSort.swap(intArray, largest, lastUnsortedIndex);
-        }
-     for (int elem : intArray) {
-         System.out.println(elem);
-     }
+        SelectionSort selectionSort = new SelectionSort(intArray);
 
     }
 }
--- src/com/leyfor/AlgorithmsInJava/SelectionSort.java
+++ src/com/leyfor/AlgorithmsInJava/SelectionSort.java
@@ -4,13 +4,39 @@ package com.leyfor.AlgorithmsInJava;
 // cuadratic O(n2)
 public class SelectionSort {
 
-    public void swap(int[] array, int i, int j) {
-        if(i == j) {
+    public SelectionSort(int[] intArray) {
+        this._initialize(intArray);
+
+    }
+
+    private void _swap(int[] array, int i, int j) {
+        if (i == j) {
             return;
         }
-
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    // Implementation for Selection Sort
+    private void _initialize(int[] intArray) {
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0; // First index
+            for (int i = 1; i <= lastUnsortedIndex; i = i + 1) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            this._swap(intArray, largest, lastUnsortedIndex);
+        }
+
+        this._display(intArray);
+    }
+
+    //Display the elements to the console
+    private void _display(int[] intArray) {
+        for (int elem : intArray) {
+            System.out.println(elem);
+        }
     }
 }
